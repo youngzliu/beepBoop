1eb8e08953cfb418740d738d68a256e097b37eb2
Young Liu <youngliu92@gmail.com>
2019-01-27 11:30:54 -0800

    add back end javascript functions for testing each of the criteria required for the website.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,46 @@
+//Back end
+var containZero = function(num){
+  if (num.toString().match(/0/) != null){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+var containOne = function(num){
+  if (num.toString().match(/1/) != null){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+var divisThree = function(num){
+  if (num % 3 === 0)
+    return true;
+  else {
+    return false;
+  }
+}
+
+var processInput = function(num){
+  if (divisThree(num)){
+    return "I'm sorry, Dave. I'm afraid I can't do that.";
+  }
+  else if (containOne(num)){
+    return "Boop!";
+  }
+  else if (containZero(num)){
+    return "Beep!";
+  }
+  else {
+    return num;
+  }
+}
+
+//Front End
 $(document).ready(function(){
   $("form#beepForm").submit(function(event){
     event.preventDefault();
